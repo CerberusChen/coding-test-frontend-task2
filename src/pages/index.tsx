b74f59e1be779434db6d9db9951619bc5b6c09f4
Elbert Chen <xiantais@hotmail.com>
2023-08-25 07:09:45 +0800

    set timer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import Countdown from "./Countdown";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return <div>hello world
+    <Countdown date={new Date('2023-09-01')} />
+  </div>;
 }
